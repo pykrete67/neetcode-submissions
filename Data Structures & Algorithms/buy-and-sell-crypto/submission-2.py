83767c66fc0d8017fa1bc68ea36edfa624f96dd4
pykrete67 <pykrete67@proton.me>
2026-07-25 22:09:56 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        
+        least = prices[0]
+        profit = 0
+
+        for n in prices:
+
+            profit = max(profit, n - least)
+
+            if n < least:
+                least = n
+
+        return profit
+                
+
+        
