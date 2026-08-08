aac8a014e90d2c5c606315e9962ee96c3b20a0f9
pykrete67 <pykrete67@proton.me>
2026-08-08 10:22:43 +0700

    Add: find-target-in-rotated-sorted-array - submission-33

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-33.py
@@ -0,0 +1,69 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # still not fully understood how the high = middle - 1, low <= high, or the high = len(nums) -1 work, need to go over this
+        # also this solution isnt the same as neetcode
+        
+        #steps to solve 1. edge case for if the list is already perfectly sorted -> just normal binary search
+        # 2. use the solution from finding min element in rotated sorted array to find the pivot point
+        #3. since we found the pivot, use if else statements to determine which of the 2 chunks to go through to find the target then just do binary search
+        low = 0
+        high = len(nums)-1
+
+        if nums[low] == target:
+            return low
+        if nums[high] == target:
+            return high
+        
+        # i came back to optimize it, no longer need a separate chunk to handle when the array is already perfectly sorted. just set low = 0 and high to last index if already sorted
+        # if nums[low] < nums[high]:
+        #     while low < high:
+        #         middle = (low+high) // 2
+        #         if nums[middle] == target:
+        #             return middle
+        #         elif nums[middle] < target:
+        #             low = middle + 1
+        #         else:
+        #             high = middle - 1
+
+        low = 0
+        high = len(nums)-1
+
+        while low < high:
+            middle = (low + high) // 2
+            if nums[middle] > nums[high]:
+                low = middle + 1
+            else:
+                high = middle
+
+        break_point = low
+
+        if target == nums[break_point]:
+            return break_point
+
+        if target == nums[break_point-1]:
+            return break_point - 1
+
+        # if else statement to check if array is already perfectly sorted
+        low = 0
+        high = len(nums)-1
+        if nums[low] < nums[high]:
+            high = len(nums)
+        else:
+            low = 0
+            high = len(nums)
+
+            if target > nums[low] and target < nums[break_point-1]:
+                high = break_point
+            else:
+                low = break_point
+
+        while low < high:
+                middle = (low+high) // 2
+                if nums[middle] == target:
+                    return middle
+                elif nums[middle] < target:
+                    low = middle + 1
+                else:
+                    high = middle
+
+        return -1
